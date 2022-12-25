87dc0c86b9f350dad9f1bc4ebca60cf0946547bf
Pavel Antonov <artplaydota@gmail.com>
2022-12-25 21:13:09 +0300

    homework

--- L16-io/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
+++ L16-io/homework/src/main/java/ru/otus/dataprocessor/FileSerializer.java
@@ -1,14 +1,27 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+
+import java.io.FileWriter;
 import java.util.Map;
 
 public class FileSerializer implements Serializer {
 
+    private final String fileName;
+
     public FileSerializer(String fileName) {
+        this.fileName = fileName;
     }
 
     @Override
     public void serialize(Map<String, Double> data) {
         //формирует результирующий json и сохраняет его в файл
+
+        try (FileWriter fos = new FileWriter(fileName)) {
+            String json = new Gson().toJson(data);
+            fos.write(json);
+        } catch (Exception e) {
+            throw new FileProcessException(e);
+        }
     }
 }
--- L16-io/homework/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ L16-io/homework/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,17 +1,36 @@
 package ru.otus.dataprocessor;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import ru.otus.model.Measurement;
 
 import java.util.List;
 
 public class ResourcesFileLoader implements Loader {
 
+    private final String fileName;
+
     public ResourcesFileLoader(String fileName) {
+        this.fileName = fileName;
     }
 
     @Override
     public List<Measurement> load() {
-
-        return null;
+        //читает файл, парсит и возвращает результат
+        try (var resourceAsStream = ResourcesFileLoader.class.getClassLoader().getResourceAsStream(fileName)) {
+            if (resourceAsStream == null) {
+                System.err.println("File not found");
+                throw new FileProcessException("File not found");
+            }
+            byte[] bytes = resourceAsStream.readAllBytes();
+            if (bytes.length == 0) {
+                System.err.println("Empty file");
+                throw new FileProcessException("Empty file");
+            }
+            return new Gson().fromJson(new String(bytes), new TypeToken<List<Measurement>>() {
+            }.getType());
+        } catch (Exception e) {
+            throw new FileProcessException(e);
+        }
     }
 }
